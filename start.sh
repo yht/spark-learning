e9f4945a961255626b37801aa55669ef3ee55836
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2024-08-12 18:48:03 +0700

    fix start.sh password

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker run --rm -p 4040:4040 -p 8888:8888 -p 8080:8080 -v .:/home/jovyan/work -e GRANT_SUDO=yes --user root ryht/spark-notebook
+docker run --rm -p 4040:4040 -p 8888:8888 -p 8080:8080 \
+  -v .:/home/jovyan/work -e GRANT_SUDO=yes --user root \
+  -e JUPYTER_TOKEN="password" ryht/spark-notebook
